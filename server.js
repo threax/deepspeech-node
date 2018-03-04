a10c428e6f3d6e777c44face6d3b8c910ff5539e
Andrew Piper <andrew@threax.com>
2018-03-03 19:06:58 -0500

    server able to translate wav, but request ends too early

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
-var http = require('http');
+const http = require('http');
+const parser = require('./parser.js');
+
+const modelBase = '/opt/deepspeech/';
+
+parser.start({
+    model: modelBase + 'output_graph.pb',
+    alphabet: modelBase + 'alphabet.txt',
+    lm: modelBase + 'lm.binary',
+    trie: modelBase + 'trie'
+});
+
 var server = http.createServer ( function(request,response){
     console.log(request.method);
     response.writeHead(200,{"Content-Type":"text\plain"});
@@ -10,14 +21,12 @@ var server = http.createServer ( function(request,response){
     {
         let body = [];
         request.on('data', (chunk) => {
-        body.push(chunk);
+            body.push(chunk);
         }).on('end', () => {
-        body = Buffer.concat(body).toString();
-            console.log(body);
+            body = Buffer.concat(body);
+            var result = parser.parse(body);
+            response.end(result);
         });
-
-
-        response.end("received POST request.");
     }
     else
     {
