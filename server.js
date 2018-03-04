9de8b763e75f3c3bf5780d8ba85071fca8821fc5
Andrew Piper <andrew@threax.com>
2018-03-04 07:49:22 -0500

    Converted server to typescript.

--- server.js
+++ server.js
@@ -1,38 +1,53 @@
-var http = require('http');
-var parser = require('./parser.js');
-var Pool = require('threads').Pool;
-var uuidv1 = require('uuid/v1');
-var pool = new Pool(1);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var http = require("http");
+var threads_1 = require("threads");
+var uuidv1 = require("uuid/v1");
+var pool = new threads_1.Pool(1);
 pool.run('./worker.js').send({}); //Warms up deep speech, but still allows web server to start quickly.
-var responses = {};
-function getResponse(index) {
-    var response = responses[index];
-    if (response) {
-        delete responses[index];
+var ResponseManager = (function () {
+    function ResponseManager() {
+        this.responses = {};
     }
-    return response;
-}
+    ResponseManager.prototype.addResponse = function (response) {
+        var name = uuidv1();
+        while (this.responses[name] !== undefined) {
+            name = uuidv1();
+        }
+        this.responses[name] = response;
+        return name;
+    };
+    ResponseManager.prototype.getResponse = function (name) {
+        var response = this.responses[name];
+        if (response) {
+            delete this.responses[name];
+        }
+        return response;
+    };
+    return ResponseManager;
+}());
+var responses = new ResponseManager();
 pool
     .on('done', function (job, message) {
-    var response = getResponse(job.sendArgs[0].responseName);
-    if (response) {
-        endRequest(200, response, message);
-    }
+    var args = job.sendArgs[0];
+    var response = responses.getResponse(args.name);
+    endRequest(200, response, message);
 })
     .on('error', function (job, error) {
     console.error('Job errored:', error);
-    var response = getResponse(job.sendArgs[0].responseName);
-    if (response) {
-        endRequest(500, response, 'Internal Server Error');
-    }
+    var args = job.sendArgs[0];
+    var response = responses.getResponse(args.name);
+    endRequest(500, response, 'Internal Server Error');
 })
     .on('finished', function () {
-    //console.log('Everything done, shutting down the thread pool.');
+    //never stop
     //pool.killAll();
 });
 function endRequest(status, response, data) {
-    response.writeHead(status, { "Content-Type": "text\plain" });
-    response.end(data);
+    if (response) {
+        response.writeHead(status, { "Content-Type": "text\plain" });
+        response.end(data);
+    }
 }
 var server = http.createServer(function (request, response) {
     if (request.method == "POST") {
@@ -40,12 +55,11 @@ var server = http.createServer(function (request, response) {
         request.on('data', function (chunk) {
             body_1.push(chunk);
         }).on('end', function () {
-            var responseName = uuidv1(); //This is still 1 thread, so this should be unique enough
-            responses[responseName] = response;
-            pool.send({
+            var args = {
                 body: Buffer.concat(body_1),
-                responseName: responseName
-            });
+                name: responses.addResponse(response)
+            };
+            pool.send(args);
         });
     }
     else {
@@ -54,5 +68,3 @@ var server = http.createServer(function (request, response) {
 });
 server.listen(80);
 console.log("Server running on port 80");
-//anohter option
-//https://stackoverflow.com/questions/46972607/node-js-receive-file-in-http-request 
--- server.ts
+++ server.ts
@@ -1,68 +1,82 @@
-const http = require('http');
-const parser = require('./parser.js');
-const Pool = require('threads').Pool;
-const uuidv1 = require('uuid/v1');
+import * as http from 'http';
+import * as parser from './parser.js';
+import { Pool } from 'threads';
+import * as uuidv1 from 'uuid/v1';
 
 const pool = new Pool(1);
 
 pool.run('./worker.js').send({}); //Warms up deep speech, but still allows web server to start quickly.
 
-var responses = {};
-function getResponse(index){
-    var response = responses[index];
-    if(response){
-        delete responses[index];
-    }
-    return response;
+interface ISendArgs {
+    body: ArrayBuffer;
+    name: string;
 }
+
+class ResponseManager {
+    private responses: { [key: string]: http.ServerResponse } = {};
+
+    public addResponse(response: http.ServerResponse): string {
+        var name = uuidv1();
+        while (this.responses[name] !== undefined) {
+            name = uuidv1();
+        }
+
+        this.responses[name] = response;
+        return name;
+    }
+
+    public getResponse(name) {
+        var response = this.responses[name];
+        if (response) {
+            delete this.responses[name];
+        }
+        return response;
+    }
+}
+
+var responses = new ResponseManager();
 
 pool
-  .on('done', function(job, message) {
-      var response = getResponse(job.sendArgs[0].responseName);
-      if(response){
+    .on('done', function (job, message) {
+        var args: ISendArgs = job.sendArgs[0];
+        var response = responses.getResponse(args.name);
         endRequest(200, response, message);
-      }
-  })
-  .on('error', function(job, error) {
-    console.error('Job errored:', error);
-    var response = getResponse(job.sendArgs[0].responseName);
-    if(response){
+    })
+    .on('error', function (job, error) {
+        console.error('Job errored:', error);
+        var args: ISendArgs = job.sendArgs[0];
+        var response = responses.getResponse(args.name);
         endRequest(500, response, 'Internal Server Error');
-    }
-  })
-  .on('finished', function() {
-    //console.log('Everything done, shutting down the thread pool.');
-    //pool.killAll();
-  });
+    })
+    .on('finished', function () {
+        //never stop
+        //pool.killAll();
+    });
 
-function endRequest(status, response, data){
-    response.writeHead(status,{"Content-Type":"text\plain"});
-    response.end(data);
+function endRequest(status: number, response: http.ServerResponse, data: string) {
+    if(response) {
+        response.writeHead(status, { "Content-Type": "text\plain" });
+        response.end(data);
+    }
 }
 
-var server = http.createServer ( function(request,response){
-    if(request.method == "POST")
-    {
+var server = http.createServer(function (request, response) {
+    if (request.method == "POST") {
         let body = [];
         request.on('data', (chunk) => {
             body.push(chunk);
         }).on('end', () => {
-            var responseName = uuidv1(); //This is still 1 thread, so this should be unique enough
-            responses[responseName] = response;
-            pool.send({
+            var args: ISendArgs = {
                 body: Buffer.concat(body),
-                responseName: responseName
-            });
+                name: responses.addResponse(response)
+            };
+            pool.send(args);
         });
     }
-    else
-    {
+    else {
         endRequest(500, response, "Undefined request .");
     }
 });
 
 server.listen(80);
 console.log("Server running on port 80");
-
-//anohter option
-//https://stackoverflow.com/questions/46972607/node-js-receive-file-in-http-request
